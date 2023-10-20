2c20e6212db0215907468c8f790f51644f205451
ryongmi <62281754+ryongmi@users.noreply.github.com>
2023-10-20 23:49:10 +0900

    fronted 개발자 하차로 인한 코드교체
    전에 사용하던 Next.js는 개발이 불가능하여 React로 변경

--- frontend/src/api/jsonplaceholderUsers.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "@/app/types";
-import { axiosInstance } from "./axiosInstance";
-
-export const jsonplaceholderUsers = async () => {
-  const { data } = await axiosInstance.get(
-    "https://jsonplaceholder.typicode.com/users"
-  );
-  return data as User[];
-};
